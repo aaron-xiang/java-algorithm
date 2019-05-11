cf0bbc7ea0c835fa78e10f7b73df98a725cd570a
Aaron Xiang <aaron.xiang@gmail.com>
2019-05-11 14:09:11 -0700

    created priority queue and max queue

---
+++ src/main/java/algo/collection/list/PriorityQueue.java
@@ -0,0 +1,4 @@
+package algo.collection.list;
+
+public interface PriorityQueue {
+}
---
+++ src/test/java/algo/collection/list/MaxPQTest.java
@@ -0,0 +1,7 @@
+package algo.collection.list;
+
+import static org.junit.Assert.*;
+
+public class MaxPQTest {
+
+}
